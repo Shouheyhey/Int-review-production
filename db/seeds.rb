74d24281f0b061a93110d0e0704f12a95bb315c4
林将平 <skuigphmb6@gmail.com>
2017-05-27 10:39:23 +0900

    get products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 require "csv"
 
-users_csv = CSV.readlines("db/users.csv")
-users_csv.shift
-users_csv.each do |row|
-  User.create(email: row[1])
+products_csv = CSV.readlines("db/r.csv")
+products_csv.shift
+products_csv.each do |row|
+  Review.create(title: row[1], image_url: row[2], created_at: row[3], detail: row[4], url: row[5]) [],)
 end
