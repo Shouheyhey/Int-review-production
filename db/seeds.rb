8e3abc5411f60c4e4a01df0afc62be1ed23ffe82
林将平 <skuigphmb6@gmail.com>
2017-05-27 12:40:40 +0900

    add review table

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,6 @@
   reviews_csv = CSV.readline("db/reviews.csv")
   reviews_csv.shift
   reviews_csv.each do |row|
-    Review.create(occupation: row[1], product_id: row[2], created_at: row[3], updated_at: row[4], user_id: row[5], rate: row[6], length: row[7], often: row[8], hourly_wage: row[9], url: row[8], rate_flexibility: row[9], rate_wage: row[10],
-    rate_future: row[11], rate_growth: row[12], rate_social: row[13], url: row[14], rate_growth: row[15], rate_value: row[16], rate_reccommendation: row[17], rate_growth: row[18], review_job: row[19], review_authority: row[20], rate_growth: row[21],
-    review_skills: row[22], review_environment: row[23], review_expectation: row[34],review_merit: row[25])
+    Review.create(occupation: row[1], product_id: row[2], created_at: row[3], updated_at: row[4], user_id: row[5], rate: row[6], length: row[7], often: row[8], hourly_wage: row[10], rate_flexibility: row[11], rate_wage: row[12],
+    rate_future: row[13], rate_social: row[14], rate_growth: row[15], rate_value: row[16], rate_reccommendation: row[17], review_job: row[18], review_authority: row[19],
+    review_skills: row[20], review_environment: row[21], review_expectation: row[22],review_merit: row[23], review_training[24])
