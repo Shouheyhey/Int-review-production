f13059f8589b19bc49495bc1c9318ffee26d56b9
林将平 <skuigphmb6@gmail.com>
2017-05-27 21:29:26 +0900

    converted revies tables into UTF-16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,7 +7,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #encoding: utf-8
-
   require "csv"
 
   products_csv = CSV.readlines("db/products.csv")
