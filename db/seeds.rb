239790d70214ff91c8801d3c8ea59550d79ccc36
林将平 <skuigphmb6@gmail.com>
2017-05-30 10:34:00 +0900

    remove span column from reviews table

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,6 @@
   reviews_csv = CSV.readlines("db/reviews.csv")
   reviews_csv.shift
   reviews_csv.each do |row|
-    Review.create(review: row[1], rate:[2], product_id: row[3], created_at: row[4], updated_at: row[5], span: row[6], user_id: row[7], occupation: row[8], length: row[9], often: row[10], hourly_wage: row[11], rate_flexibility: row[12],
-    rate_wage: row[13],rate_future: row[14],rate_growth: row[15], rate_social: row[16], rate_value: row[17],rate_reccommendation: row[18],  review_job: row[19], review_authority: row[20],review_skills: row[21],review_training: row[22], review_environment: row[23], review_expectation: row[24],review_merit: row[25])
+    Review.create(review: row[1], rate:[2], product_id: row[3], created_at: row[4], updated_at: row[5], user_id: row[6], occupation: row[7], length: row[8], often: row[9], hourly_wage: row[10], rate_flexibility: row[11],
+    rate_wage: row[12],rate_future: row[13],rate_growth: row[14], rate_social: row[15], rate_value: row[16],rate_reccommendation: row[17],  review_job: row[18], review_authority: row[19],review_skills: row[20],review_training: row[21], review_environment: row[22], review_expectation: row[23],review_merit: row[24])
   end
