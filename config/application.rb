29c74c9bfaba4d98618c041c8f4ce2fc2c8a15b7
林将平 <skuigphmb6@gmail.com>
2017-05-31 12:10:56 +0900

    addign require 'letter_opener' if Rails.env.development? in application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'letter_opener'
-
 module TechReviewSite
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
