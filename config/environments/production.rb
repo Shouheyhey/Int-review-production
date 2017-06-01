aa8c2175fe10b084cfe4164ada1099f05254dcfe
林将平 <skuigphmb6@gmail.com>
2017-06-01 17:53:05 +0900

    final check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 TechReviewSite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+    # GA.tracker = "UA-100338312-1"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
