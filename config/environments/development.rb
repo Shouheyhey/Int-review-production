8c61e432d67e8de65a3024c97baf78d7bd83b86d
林将平 <skuigphmb6@gmail.com>
2017-06-01 11:51:02 +0900

    change localhost to heroku URL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ TechReviewSite::Application.configure do
   # config.action_mailer.default_url_options   = { host: 'localhost:3000', port: 3000  }
   # # config.action_mailer.delivery_method = :letter_opener
   #
-  config.action_mailer.default_url_options = { host: 'localhost:3000', port: 3000 }
+  config.action_mailer.default_url_options = {host: 'http:/salty-eyrie-20337.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
