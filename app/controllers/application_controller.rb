306aea8c2e2b23f9fcd08ea8e6b8bad77e98ac97
林将平 <skuigphmb6@gmail.com>
2017-06-03 12:15:52 +0900

    add  https://salty-eyrie-20337.herokuapp.com/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ def ensure_domain
  return unless /\.herokuapp.com/ =~ 'int-review.jp'
 
  # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
- port = ":#{request.port}" unless [80, 443].include?(request.port)
+ port = ":#{'https://salty-eyrie-20337.herokuapp.com/'}" unless [80, 443].include?(request.port)
  redirect_to "#{'http://'}#{'int-review.jp'}#{port}#{request.uri}", status: :moved_permanently
  # パラメタが必要な場合は、request.fullpath、切りたい場合は request.path
 end
