46a4444f10e8b524bb3583b6bf5dea32ca923af5
林将平 <skuigphmb6@gmail.com>
2017-06-03 12:44:46 +0900

    change http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :ensure_domain
-  before_filter :deactivate_ssls
+  before_filter :deactivate_ssl
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
