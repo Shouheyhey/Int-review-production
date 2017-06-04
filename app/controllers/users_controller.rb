a3dc6b061e96512434889ca89d3ee8c22349beb7
林将平 <skuigphmb6@gmail.com>
2017-06-04 20:57:04 +0900

    rmv show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
-# class UsersController < ApplicationController
-#   def show
-#     @user = Users.find(params[:id])
-#   end
-#
-# end
+class UsersController < ApplicationController
+  def show
+  end
+end
