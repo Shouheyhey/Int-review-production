63022e9b64ec826a85d8dc3d45799d62cd9386b0
林将平 <skuigphmb6@gmail.com>
2017-06-05 17:26:29 +0900

    delete GA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 IntReviewProduct::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # トラッキングIDを記載
-      # GA.tracker = "UA-100338312-1"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
