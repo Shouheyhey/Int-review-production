50ceb89ddc48a01bcccb02267c98390af11dc1b3
林将平 <skuigphmb6@gmail.com>
2017-06-05 13:15:13 +0900

    change name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-before_action :after_sign_up_path_for
 
   def after_sign_up_path_for(resource)
-    pages_notification_path(resource)
+    pages_new_registration_notification
   end
 end
