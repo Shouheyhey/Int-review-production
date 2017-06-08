5972f6dffe43f60c270e4b66de2b705507eb80c8
林将平 <skuigphmb6@gmail.com>
2017-06-08 16:41:03 +0900

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,98 +1,60 @@
 IntReviewProduct::Application.configure do
-
-  GA.tracker = "UA-100338312-1"
-  
   # Settings specified here will take precedence over those in config/application.rb.
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+  # トラッキングIDを記載
+  GA.tracker = "UA-100338312-1"
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
 
+  # Do not eager load code on boot.
+  config.eager_load = false
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = false
-
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
-
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
-
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving Xof images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  # 結果的にdevelopment.rb上でconfigを設定すると、クラッシュを起こしてしまう。
+  # # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_url_options = { :host => 'https://salty-eyrie-20337.herokuapp.com/' }
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-config.action_mailer.default_url_options = {host: 'http://int-review.jp/' }
-  config.action_mailer.raise_delivery_errors = false #この一文も追記!!
+  # config.action_mailer.default_url_options   = { host: 'localhost:3000', port: 3000  }
+  # # config.action_mailer.delivery_method = :letter_opener
+  #
+  config.action_mailer.default_url_options = {host: 'http://int-review.jp/' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => "app69354248@heroku.com",
-      :password => "pokqqpby6815",
-      :domain => "heroku.com",
-      :address => "smtp.sendgrid.net",
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "int.review.jp@gmail.com",
+    :password => "intre0123",
+    :authentication => 'login'
+}
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   user_name:             'int.review.jp@gmail.com',
+  #   password:              ‘intre0123’,
+  #   authentication:        'login',
+  #   enable_starttls_auto:  true
+  # }
+  #
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  #  config.assets.compile = true
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+
 
 end
