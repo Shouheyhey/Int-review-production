03c3343e88ad596b940f93f10bcd73353b26b967
林将平 <skuigphmb6@gmail.com>
2017-06-08 12:48:14 +0900

    GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 IntReviewProduct::Application.configure do
+
+  GA.tracker = "UA-100338312-1"
+  
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
