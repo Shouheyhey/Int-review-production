02d6850883672c4df82390a3673a059bf95395cc
林将平 <skuigphmb6@gmail.com>
2017-06-08 09:44:33 +0900

    edit asset files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,15 +18,3 @@
 //= require highcharts/highcharts
 //= require highcharts/highcharts-more
 //= require highcharts/highstock
-
-document.addEventListener("turbolinks:load", function() {
-  $('#tab-menu li').on('click', function(){
-    if($(this).not('selected')){
-      // タブメニュー
-      $(this).addClass('selected').siblings('li').removeClass('selected');
-      // タブの中身
-      var index = $('#tab-menu li').index(this);
-      $('#tab-box div').eq(index).addClass('selected').siblings('div').removeClass('selected');
-    }
-  });
-});
---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,12 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+//You can use CoffeeScript in this file: http://coffeescript.org/
+document.addEventListener('turbolinks:load', function() {
+  $('#tab-menu li').on('click', function(){
+    if($(this).not('selected')){
+      $(this).addClass('selected').siblings('li').removeClass('selected');
+      var index = $('#tab-menu li').index(this);
+      $('#tab-box div').eq(index).addClass('selected').siblings('div').removeClass('selected');
+    }
+  });
+});
