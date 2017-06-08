bba65955f2015405ac87669bee2bae6c22f78264
林将平 <skuigphmb6@gmail.com>
2017-06-08 14:30:37 +0900

    private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   # before_filter :deactivate_ssl
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:kanji_name, :furigana_name,:birthday, :sex, :address, :university, :department, :culture_or_science,:graduation_year,:company, :others] )
   end
