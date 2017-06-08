94d9bc0f852b6ef19c4ca23bdb4d8e3df5b4309b
林将平 <skuigphmb6@gmail.com>
2017-06-08 14:41:07 +0900

    permit clear

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,19 @@ class ApplicationController < ActionController::Base
   # before_filter :deactivate_ssl
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:kanji_name, :furigana_name,:birthday, :sex, :address, :university, :department, :culture_or_science,:graduation_year,:company, :others] )
-  end
-
   def after_sign_out_path_for(resources)
     '/'
   end
+
+
+  def configure_permitted_parameters
+      registration_params =  [:kanji_name, :furigana_name,:birthday, :sex, :address, :university, :department, :culture_or_science,:graduation_year,:company, :others]
+
+  if params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) do
+        |u| u.permit(registration_params)
+      end
+    end
+end
 
 end
